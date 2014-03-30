523e98aa46f020465fd40cbc0093febfe9820640
kamyar <kkamyarr@cs.ubc.ca>
2014-03-30 12:36:58 -0700

    minor
    Change-Id: I41c23affabfd56938cb4a174e20f2d46b652d9b0

---
+++ src/ca/ubc/ca/ephemerallauncherexperiment/deprecated/Condition.java
@@ -0,0 +1,31 @@
+package ca.ubc.ca.ephemerallauncherexperiment.deprecated;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class Condition extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_condition);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.condition, menu);
+		return true;
+	}
+	
+	public void startTrial(View view){
+		
+	Intent intent = new Intent(this, Trial.class);
+	startActivity(intent);
+	
+	}
+
+}
