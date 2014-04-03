bc69707af80b7bcb8415a4b5344baadbb5be2072
kamyar <kkamyarr@cs.ubc.ca>
2014-04-03 13:10:32 -0700

    - moved file test to FileManager.java
    Change-Id: I5272d77f77e237e89b43c9ae882c93633d025ec8

--- src/ca/ubc/cs/ephemerallauncherexperiment/FileManager.java
+++ src/ca/ubc/cs/ephemerallauncherexperiment/FileManager.java
@@ -1,5 +1,38 @@
 package ca.ubc.cs.ephemerallauncherexperiment;
 
-public class FileManager {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import android.content.Context;
+import android.util.Log;
+
+public class FileManager {
+	
+	public static void testSavingToSdCard(Context c) {
+		String filename = "file1";
+		boolean ifAppendData = true;
+		File file = new File(getExtStorageDir(c, "dir1"),filename);
+		
+		try {
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(), ifAppendData);
+			BufferedWriter bw = new BufferedWriter(fw);	
+			bw.write(R.string.file_test);
+			bw.flush();
+			bw.close(); }
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static File getExtStorageDir(Context context, String dirName) {
+	     
+	    File file = new File(context.getExternalFilesDir(
+	            "EXPERIMENT_DATA"), dirName);
+	    if (!file.mkdirs()) {
+	        Log.e("LOG", "Directory not created");
+	    }
+	    return file;
+	}
 }
