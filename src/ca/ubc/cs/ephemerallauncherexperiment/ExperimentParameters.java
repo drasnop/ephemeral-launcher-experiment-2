88ee611e06c9a7fd531418efd7d2e3bbc72050a7
antoine <aponsard@cs.ubc.ca>
2014-04-13 18:42:16 -0700

    "solved" the problem of the messed-up layout, by cleaning the project...

--- src/ca/ubc/cs/ephemerallauncherexperiment/ExperimentParameters.java
+++ src/ca/ubc/cs/ephemerallauncherexperiment/ExperimentParameters.java
@@ -6,14 +6,14 @@ import android.graphics.Color;
 public class ExperimentParameters {
 	
 	public static final int NUM_CONDITIONS=5;
-	public static final int NUM_TRIALS=2;		//20
+	public static final int NUM_TRIALS=5;		//20
 	public static final int NUM_PARTICIPANTS=20;
 	
 	public static final int zipfSize = 20;
 	public static final int NUM_PAGES = 3;
 	public static final int NUM_HIGHLIGHTED_ICONS = 9;
 	
-	public static final double MIN_ACCURACY = .90;
+	public static final double MIN_ACCURACY = .80;
 	
 	public static final int TRIAL_TIMEOUT_MS = 20000;	//10000	
 	public static final int TIMEOUT_CHECK_INTERVAL = 500;
